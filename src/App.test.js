aff1f8ef6c59aa3ca16f9d532bb6fb992f1baed9
Stephen S <sparlingcreations@gmail.com>
2022-06-23 21:40:10 -0400

    add tests for react-ga4

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,51 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { event } from 'react-ga4';
+
+jest.mock('react-ga4', () => {
+  return {
+    ...jest.requireActual('react-ga4'),
+    event: jest.fn(),
+  };
+});
 
 describe('App tests', () => {
-  it('properly increments the displayed value on click of the button', () => {
-    render(<App />);
-    const clickAmount = screen.getByTestId('click-amount');
-    expect(clickAmount).toHaveTextContent('0');
-    const incrementButton = screen.getByRole('button');
-    fireEvent.click(incrementButton);
-    const updatedClickAmount = screen.getByTestId('click-amount');
-    expect(updatedClickAmount).toHaveTextContent('1');
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('base functionality', () => {
+    it('properly increments the displayed value on click of the button', () => {
+      render(<App />);
+      const clickAmount = screen.getByTestId('click-amount');
+      expect(clickAmount).toHaveTextContent('0');
+      const incrementButton = screen.getByRole('button');
+      fireEvent.click(incrementButton);
+      const updatedClickAmount = screen.getByTestId('click-amount');
+      expect(updatedClickAmount).toHaveTextContent('1');
+    });
+  });
+
+  describe('Analytics callouts', () => {
+    it('should run the "increment_button_clicked" GA callout when the button is clicked', () => {
+      render(<App />);
+      const incrementButton = screen.getByRole('button');
+      fireEvent.click(incrementButton);
+      expect(event).toHaveBeenCalledTimes(1);
+    });
+
+    it('should run the "increment_button_clicked" GA callout with the correct properties', () => {
+      render(<App />);
+      const incrementButton = screen.getByRole('button');
+      fireEvent.click(incrementButton);
+      expect(event).toHaveBeenCalledWith('increment_button_clicked', {
+        click_amount: 1,
+      });
+
+      fireEvent.click(incrementButton);
+      expect(event).toHaveBeenCalledWith('increment_button_clicked', {
+        click_amount: 2,
+      });
+    });
   });
 });
