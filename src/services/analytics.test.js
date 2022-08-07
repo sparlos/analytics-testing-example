f373a81a45371798cff74c8bf06b6030a74c45e9
Stephen S <sparlingcreations@gmail.com>
2022-08-07 17:47:45 -0400

    add tests for analytics service

---
+++ src/services/analytics.test.js
@@ -0,0 +1,32 @@
+import { makeAnalyticsCall } from './analytics';
+import ReactGA from 'react-ga4';
+
+jest.mock('react-ga4', () => ({
+  ...jest.requireActual('react-ga4'),
+  event: jest.fn(),
+}));
+
+describe('(Service) analytics', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('makeAnalyticsCall', () => {
+    it('should make a call to Google Analytics via the react-ga4 package', () => {
+      makeAnalyticsCall();
+      expect(ReactGA.event.mock.calls.length).toBe(1);
+    });
+
+    it('should make a call to Google Analytics with the correct event name and options', () => {
+      const mockEventName = 'test-event';
+      const mockEventOptions = {
+        hello: 'world',
+      };
+
+      makeAnalyticsCall(mockEventName, mockEventOptions);
+      expect(ReactGA.event.mock.calls.length).toBe(1);
+      expect(ReactGA.event.mock.calls[0][0]).toBe(mockEventName);
+      expect(ReactGA.event.mock.calls[0][1]).toStrictEqual(mockEventOptions);
+    });
+  });
+});
